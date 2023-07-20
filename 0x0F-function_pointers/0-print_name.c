aee9d57ed260d18da16ae7f5693b250aa8664e57
Neo <neomodiba14@gmail.com>
2023-07-20 04:58:12 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,9 @@
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
- **/
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
